73469b037a83ac3148e3940f39ab1a85b3102f4f
Florencia Rodriguez <florenrodrigue9@gmail.com>
2021-01-24 21:38:03 -0300

    subo detalle de producto

--- cfa/app.js
+++ cfa/app.js
@@ -4,8 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var detalleRouter = require ('./routes/detalle')
+
 
 var app = express();
 
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/detalle' , detalleRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
