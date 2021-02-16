5f0de4d92af4634427b2663cef69056fc946fea9
Jony <jonathan_rivarola_91@hotmail.com>
2021-02-15 23:25:59 -0300

    agrego productos y categorias

--- cfa/controllers/productosController.js
+++ cfa/controllers/productosController.js
@@ -1,4 +1,21 @@
+const productos = require('../data/productos');
+
 const productosController = {
+    productos:(req,res) =>{
+        let categorias = [] 
+        
+        productos.forEach(producto => {
+            categorias.push(producto.categoria)
+        });
+
+        const categoriasArr = new Set(categorias) //creo nuevo array con valores unicos
+        let categoriasList = [...categoriasArr] //guardo en el nuevo array los valores unicos
+
+        res.render('productos',{
+            productos,
+            categoriasList
+        })
+    },
     carga: (req,res) =>{
         res.render('cargaProducto')
     },
@@ -7,6 +24,32 @@ const productosController = {
     },
     detalle: (req,res) =>{
         res.render('detalle')
+    },
+    categoria: (req,res) =>{
+        let categ = req.params.categ;
+        let productosCateg = [];
+        
+        productos.forEach(producto => {
+            if(producto.categoria == categ){
+                productosCateg.push(producto)
+            }
+        });
+
+        
+        let categorias = [] 
+        
+        productos.forEach(producto => {
+            categorias.push(producto.categoria)
+        });
+
+        const categoriasArr = new Set(categorias) //creo nuevo array con valores unicos
+        let categoriasList = [...categoriasArr] //guardo en el nuevo array los valores unicos
+
+        res.render('categoria',{
+            productos,
+            categoriasList,
+            productosCateg
+        })
     }
 }
 
--- cfa/routes/productosRouter.js
+++ cfa/routes/productosRouter.js
@@ -3,7 +3,9 @@ var router = express.Router();
 const productosController =require('../controllers/productosController')
 
 /* GET home page. */
+router.get('/', productosController.productos);
 router.get('/carga', productosController.carga);
 router.get('/carrito', productosController.carrito);
 router.get('/detalle', productosController.detalle);
+router.get('/categoria/:categ', productosController.categoria);
 module.exports = router;
