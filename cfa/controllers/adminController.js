d79d47b1a1adbfcde639a8b086296a837d8d2df1
Jony <jonathan_rivarola_91@hotmail.com>
2021-02-17 15:49:01 -0300

    actualizo admin

--- cfa/controllers/adminController.js
+++ cfa/controllers/adminController.js
@@ -1,15 +1,90 @@
+const productos = require('../data/productos');
+
 module.exports = {
+    index: (req,res) => {
+        res.render('admin/index');
+    },
+    categoria: (req,res) =>{
+        let categ = req.params.categ;
+        let productosCateg = [];
+        
+        productos.forEach(producto => {
+            if(producto.categoria == categ){
+                productosCateg.push(producto)
+            }
+        });
+
+        
+        let categorias = [] 
+        
+        productos.forEach(producto => {
+            categorias.push(producto.categoria)
+        });
+
+        const categoriasArr = new Set(categorias) //creo nuevo array con valores unicos
+        let categoriasList = [...categoriasArr] //guardo en el nuevo array los valores unicos
+
+        res.render('admin/categoria',{
+            productos,
+            categoriasList,
+            productosCateg
+        })
+    },
+    buscar: (req,res)=>{
+        const buscar = req.query.buscar;
+        
+        let categorias = [] 
+        
+        productos.forEach(producto => {
+            categorias.push(producto.categoria)
+        });
+
+        const categoriasArr = new Set(categorias) //creo nuevo array con valores unicos
+        let categoriasList = [...categoriasArr] //guardo en el nuevo array los valores unicos
+
+        const resultado = productos.filter(producto=>{
+            return producto.name.toLowerCase().includes(buscar)
+        })
+        res.render('admin/productos',{
+            categoriasList,
+            productos : resultado
+        })
+    },
     listarProducto: (req,res) => {
-        res.render('admin/productos');
+        let categorias = [] 
+        
+        productos.forEach(producto => {
+            categorias.push(producto.categoria)
+        });
+
+        const categoriasArr = new Set(categorias) //creo nuevo array con valores unicos
+        let categoriasList = [...categoriasArr] //guardo en el nuevo array los valores unicos
+
+        res.render('admin/productos',{
+            productos,
+            categoriasList
+        })
     },
     crearProducto: (req,res) => {
-
+        res.render('admin/cargaProducto');
     },
-    guardarProducto: (req,res) => {
+    guardarProducto: (req,res) => { //store
+        let lastID = 1;
+        productos.forEach(producto =>{
+            if (producto.id > lastID){
+                lastID = producto.id
+            }
+        });
+
+        const {img, nombre, detalle, precio, oferta, categoria} = req.body;
+
+        let producto = {
+            
+        }
 
     },
     editarProducto: (req,res) => {
-
+        res.render('admin/editarProducto');
     },
     actualizarProducto: (req,res) => {
 
--- cfa/routes/adminRouter.js
+++ cfa/routes/adminRouter.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
-const {actualizarProducto,borrarProducto,crearProducto,editarProducto,guardarProducto,listarProducto} = require('../controllers/adminController')
+const {actualizarProducto,borrarProducto,crearProducto,editarProducto,guardarProducto,listarProducto,index,categoria,buscar} = require('../controllers/adminController')
+
+router.get('/',index);
+router.get('/productos/categoria/:categ', categoria);
+router.get('/productos/buscar', buscar);
 
 router.get('/productos', listarProducto);
 
 router.get('/productos/carga', crearProducto);
-router.post('/productos/guardar', guardarProducto);
+router.post('/productos/guardar', guardarProducto);//store
 
 router.get('/productos/editar/:id', editarProducto);
 router.put('/productos/actualizar/:id', actualizarProducto);
