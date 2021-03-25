89b7ea2cf02d74505e973109b3d4b7d9583bafee
Florenrodriguez97 <Florenrodrigue9@gmail.com>
2021-03-25 16:08:25 -0300

    Registro validation}

--- cfa/public/javascripts/app.js
+++ cfa/public/javascripts/app.js
@@ -3,51 +3,49 @@ const qs = (e) => document.querySelector(e)
 window.addEventListener('load', () => {
     console.log('Javascript está vinculado correctamente');
 
-    let formulario = qs('#form');
+    let Foto_X = document.getElementById("Foto_X");
+    let Email = document.getElementById("Email");
+    let Password = document.getElementById("Password");
+    let Password2 = document.getElementById("Password2");
+    let Name = document.getElementById("Name");
+    let Last_name = document.getElementById("Last_name");
+  
+ console.log(Email)
 
-    console.log(formulario)
-
-    let inputFoto_x = formulario;
-    let inputEmail = formulario;
-    let inputPassword = formulario;
-    let inputPassword2 = formulario;
-    let inputName = formulario;
-    let inputLast_name = formulario;
-
-    (inputFoto_x.value) ? inputFoto_x.value = "" : null
+    /*(inputFoto_x.value) ? inputFoto_x.value = "" : null */
     let regExLetras = /^[a-zA-Z\sñáéíóúü]*$/
     let regExEmail = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]:+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/
     let regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/
     let regExExt = /(.jpg|.jpeg|.png|.gif|.webp)$/i;
 
 
-        inputFoto_x.addEventListener('blur', function () {
+       Foto_x.addEventListener('blur', function () {
             switch (true) {
                 case !inputFoto_x.value:
                     errorFoto_x.innerHTML = "Este campo es obligatorio"
-                    inputFoto_x.classList.add('is-invalid')
+                   Foto_x.classList.add('is-invalid')
                     break
                 default:
-                    inputFoto_x.classList.remove('is-invalid');
-                    inputFoto_x.classList.add('is-valid');
+                   Foto_x.classList.remove('is-invalid');
+                   Foto_x.classList.add('is-valid');
                     errorFoto_x.innerHTML = "";
             }
         })
-        inputFoto_x.addEventListener('change', (e) => {
+        Foto_x.addEventListener('change', (e) => {
             switch (true) {
-                case !regExExt.exec(inputFoto_x.value):
+                case !regExExt.exec(Foto_x.value):
                     errorFoto_x.innerHTML = "Solo imágenes con extensión jpg, jpeg, png, gif, webp"
-                    inputFoto_x.classList.add('is-invalid')
+                    Foto_x.classList.add('is-invalid')
                     vistaPrevia.src = ""
                     break;
-                case inputFoto_x.files[0].size > oneMB * 2:
+                case Foto_x.files[0].size > oneMB * 2:
                     errorFoto_x.innerHTML = "El archivo debe pesar menos de 2Mb"
-                    inputFoto_x.classList.add('is-invalid')
+                    Foto_x.classList.add('is-invalid')
                     vistaPrevia.src = ""
                     break
                 default:
-                    inputFoto_x.classList.remove('is-invalid');
-                    inputFoto_x.classList.add('is-valid');
+                    Foto_x.classList.remove('is-invalid');
+                    Foto_x.classList.add('is-valid');
                     errorFoto_x.innerHTML = "";
                     let reader = new FileReader();
                     reader.readAsDataURL(e.target.files[0])
@@ -58,95 +56,95 @@ window.addEventListener('load', () => {
             }
         })
 
-    inputEmail.addEventListener('blur', () => {
+    Email.addEventListener('blur', () => {
         switch (true) {
-            case !inputEmail.value:
+            case !Email.value:
                 errorEmail.innerHTML = "El campo email es obligatorio";
-                inputEmail.classList.add('is-invalid');
+                Email.classList.add('is-invalid');
                 break;
-                case !regExEmail.test(inputEmail.value):
+                case !regExEmail.test(Email.value):
                     errorEmail.innerHTML = ""
-                    inputEmail.classList.add('is-invalid');
+                    Email.classList.add('is-invalid');
                     break
             default:
                 errorEmail.innerHTML = "";
-                inputEmail.classList.remove('is-invalid');
-                inputEmail.classList.add('is-valid');
+                Email.classList.remove('is-invalid');
+                Email.classList.add('is-valid');
                 break;
         }
 
     })
 
-    inputPassword.addEventListener('blur', () => {
+    Password.addEventListener('blur', () => {
         switch (true) {
-            case !inputPassword.value:
+            case !Password.value:
                 errorPassword.innerHTML = "El campo contraseña es obligatorio";
-                inputPassword.classList.add('is-invalid');
+                Password.classList.add('is-invalid');
                 break;
-                case !regExPass.test(inputPassword.value):
-                    errorPassword.innerHTML = "La contraseña debe tener entre 6 y 12 caracteres"
-                    inputPassword.classList.add('is-invalid');
+                case !regExPass.test(Password.value):
+                    errorPassword.innerHTML = "La contraseña debe tener entre 6 y 8 caracteres"
+                    Password.classList.add('is-invalid');
                     break
             default:
                 errorPassword.innerHTML = "";
-                inputPassword.classList.remove('is-invalid');
-                inputPassword.classList.add('is-valid');
+                Password.classList.remove('is-invalid');
+                Password.classList.add('is-valid');
                 break;
         }
 
     })
-    inputPassword2.addEventListener('blur', () => {
+    Password2.addEventListener('blur', () => {
         switch (true) {
-            case !inputPassword2.value:
+            case !Password2.value:
                 errorPassword2.innerHTML = "El campo contraseña es obligatorio";
-                inputPassword2.classList.add('is-invalid');
+                Password2.classList.add('is-invalid');
                 break;
-                case !regExPass.test(inputPassword2.value):
-                    errorPassword2.innerHTML = "La contraseña debe tener entre 6 y 12 caracteres"
-                    inputPassword2.classList.add('is-invalid');
+                case !regExPass.test(Password2.value):
+                    errorPassword2.innerHTML = "La contraseña debe tener entre 6 y 8 caracteres"
+                    Password2.classList.add('is-invalid');
                     break
             default:
                 errorPassword2.innerHTML = "Las contraseñas no coinciden";
-                inputPassword2.classList.remove('is-invalid');
-                inputPassword2.classList.add('is-valid');
+                Password2.classList.remove('is-invalid');
+                Password2.classList.add('is-valid');
                 break;
         }
 
     })
     
 
-    inputName.addEventListener('blur', () => {
+    Name.addEventListener('blur', () => {
         switch (true) {
-            case !inputName.value:
+            case !Name.value:
                 errorName.innerHTML = "El campo nombre es obligatorio";
-                inputName.classList.add('is-invalid');
+                Name.classList.add('is-invalid');
                 break;
-                case !regExLetras.test(inputName.value):
-                    errorname.innerHTML = "Solo letras"
-                    inputName.classList.add('is-invalid');
+                case !regExLetras.test(Name.value):
+                    errorName.innerHTML = "Solo letras"
+                    Name.classList.add('is-invalid');
                     break
             default:
                 errorName.innerHTML = "";
-                inputName.classList.remove('is-invalid');
-                inputName.classList.add('is-valid');
+                Name.classList.remove('is-invalid');
+                Name.classList.add('is-valid');
                 break;
         }
 
     })
-    inputLast_name.addEventListener('blur', () => {
+    Last_name.addEventListener('blur', () => {
         switch (true) {
-            case !inputLast_name.value:
+            case !Last_name.value:
                 errorLast_name.innerHTML = "El campo apellido es obligatorio";
-                inputLast_name.classList.add('is-invalid');
+                Last_name.classList.add('is-invalid');
                 break;
-                case !regExLetras.test(inputLast_name.value):
+                case !regExLetras.test(Last_name.value):
                     errorLast_name.innerHTML = "Solo letras"
-                    inputLast_name.classList.add('is-invalid');
+                    Last_name.classList.add('is-invalid');
                     break
             default:
                 errorLast_name.innerHTML = "";
-                inputLast_name.classList.remove('is-invalid');
-                inputLast_name.classList.add('is-valid');
+                Last_name.classList.remove('is-invalid');
+                Last_name.classList.add('is-valid');
                 break;
         }
 
--- cfa/validations/registroValidator.js
+++ cfa/validations/registroValidator.js
@@ -1,42 +1,44 @@
 const fs = require('fs');
 const {check, body} = require('express-validator');
-const users_db=JSON.parse(fs.readFileSync('./data/usuarios.json'))
+const db = require ('./database/models');
 
 module.exports = [
-    check('nombre')
-    .notEmpty().withMessage('El nombre de usuario es requerido'),
 
-    check('apellido')
-    .notEmpty().withMessage('El apellido es requerido'),
-    
+    check('avatar')
+    .notEmpty().withMessage('La imagen es requerida'),
+
     check('email')
     .notEmpty().withMessage('El email es requerido')
     .isEmail().withMessage('El email debe ser valido'),
     body('email').custom(value => {
-        let result = users_db.find(usuario => usuario.email === value.trim());
-        if(result){
-            return false
-        }else{
-            return true
-        }
+        return db.User.findOne({
+            where : {
+                email : value
+            }
+        })
+        .then(user => {
+            if(user){
+                return Promise.reject('Este email ya está registrado')
+            }
+        })
     })
     .withMessage('El email ya esta registrado'),
 
-    check('pass')
+    check('password')
     .notEmpty().withMessage('La contraseña es requerida')
     .isLength({
         min:6, 
-        max:12
+        max:8
     })
-    .withMessage('La contraseña debe tener un mínimo de 6 y un máximo de 12'),
+    .withMessage('La contraseña debe tener un mínimo de 6 y un máximo de 8'),
 
-    body('pass2').custom((value,{req}) => {
-        if(value !== req.body.pass){
-            return false
-        }else{
-            return true
-        }
-    })
-    .withMessage('Las contraseñas no coinciden')
+    body('password2').custom((value,{req}) => value!== req.body.pass ? false : true)
+    .withMessage('Las contraseñas no coinciden'),
+
+    check('name')
+    .notEmpty().withMessage('El nombre de usuario es requerido'),
+
+    check('last_name')
+    .notEmpty().withMessage('El apellido es requerido'),
 
 ]
